088f423dd16e30ff45b7dcdaf98c1e2b9376cfa1
NuriAmari <nuri.amari99@gmail.com>
2018-11-25 05:09:34 -0500

    set up board

---
+++ index.js
@@ -0,0 +1,22 @@
+var board = ChessBoard('board');
+
+/*var board, game = new Chess();
+
+while (!game.game_over()) {
+  var moves = game.moves();
+  var move = moves[Math.floor(Math.random() * moves.length)];
+  board.move(move);
+}
+
+var cfg = {
+    draggable: true,
+    position: 'start',
+    onDragStart: onDragStart,
+    onDrop: onDrop,
+    onMouseoutSquare: onMouseoutSquare,
+    onMouseoverSquare: onMouseoverSquare,
+    onSnapEnd: onSnapEnd
+};
+
+board = ChessBoard(board, cfg);
+console.log(chess.pgn());*/
